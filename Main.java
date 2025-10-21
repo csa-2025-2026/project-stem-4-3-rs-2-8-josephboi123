2966786601e8bf401b009c6cf77cffcfcec30d5e
josephboi123 <jevaldez101@student.hbuhsd.edu>
2025-10-21 18:19:55 +0000

    control

--- Main.java
+++ Main.java
@@ -5,5 +5,17 @@ public class Main
   public static void main(String[] args)
   {
     // write solutions to problems here
+    
+    
+    for (int oddnumber = 1; oddnumber <= 25; oddnumber +=2)
+
+
+        {
+         System.out.println(oddnumber);
+         }
+
+
+
+
   }
 }
